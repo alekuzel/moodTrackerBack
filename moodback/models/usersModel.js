b5a1be93c7efe0582dabee9f3c2d6629d4897859
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-16 23:13:18 +0100

    password hashing

--- moodback/models/usersModel.js
+++ moodback/models/usersModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator'); //plug in for making sure email (or anything else) is unique
+const bcrypt = require('bcrypt'); //use it for hashing passwords
 
 // Define your schema as normal.
 const userSchema = mongoose.Schema({
@@ -12,7 +13,19 @@ const userSchema = mongoose.Schema({
 // Apply the uniqueValidator plugin to userSchema.
 userSchema.plugin(uniqueValidator);
 
+// here we hash all the password and then save them
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
 
+  try {
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 
 // Define User model
