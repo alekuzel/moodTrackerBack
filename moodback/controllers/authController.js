a223612dc8d2af51e1fc2a5a04cb1023df8bc1a7
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-28 15:28:17 +0100

    commit

--- moodback/controllers/authController.js
+++ moodback/controllers/authController.js
@@ -1,20 +1,21 @@
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/usersModel');
 
 async function loginUser(req, res) {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if (!user) return res.status(404).send('User not found');
+  if (!user) return res.status(404).json({ error: 'User not found' });
   
   const validPassword = await bcrypt.compare(password, user.password);
-  if (!validPassword) return res.status(401).send('Invalid password');
+  if (!validPassword) return res.status(401).json({ error: 'Invalid password' });
 
   // Generate JWT token
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
   // Send JSON response with token
-  res.json({ token });
+  res.status(200).json({ token });
 }
+
 
 module.exports = { loginUser };
