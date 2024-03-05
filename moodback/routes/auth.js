e69dd2d3f334bce60777d0efb9ef08d7cff3acd7
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-03-05 15:20:39 +0100

    commit

--- moodback/routes/auth.js
+++ moodback/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Importing loginUser function from authController
 const { loginUser } = require('../controllers/authController');
 
-// Correct usage of post method with loginUser as a callback function
+
 router.post('/login', loginUser);
 
 module.exports = router;
--- moodback/routes/moods.js
+++ moodback/routes/moods.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const moodsController = require('../controllers/moodsController');
 
-// Define routes for notes
+// Define routes 
 router.get('/', moodsController.getAllMoods);
-router.get('/users/:userid', moodsController.getMoodsByUserId); // Move this line up
+router.get('/users/:userid', moodsController.getMoodsByUserId); 
 router.get('/:id', moodsController.getMoodById);
 router.post('/', moodsController.createMood);
 router.put('/:id', moodsController.updateMood);
